5be36cfa535653ddcef6fb036253b32b7dfc2708
Raiman <wijayakaleb@gmail.com>
2019-05-15 14:38:29 +0700

    Beta 1.1 80%

--- AnimateBubbleFade/ViewController.swift
+++ AnimateBubbleFade/ViewController.swift
@@ -12,11 +12,13 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var bubble: UIView!
     var flag:Bool = true
+    var red = CGFloat(Float.random(in: 0...1))
+    var blue = CGFloat(Float.random(in: 0...1))
+    var green = CGFloat(Float.random(in: 0...1))
     
     override func viewDidLoad() {
         super.viewDidLoad()
         bubble.layer.cornerRadius = bubble.frame.width/2
-        
         let gesture = UITapGestureRecognizer(target: self, action:  #selector(self.checkAction))
         self.bubble.addGestureRecognizer(gesture)
         
@@ -24,14 +26,13 @@ class ViewController: UIViewController {
     
     func fadeOut(view: UIView){
         view.transform = CGAffineTransform(scaleX: 5, y: 5)
-        view.backgroundColor = UIColor(displayP3Red: CGFloat(Float.random(in: 0...1)), green: CGFloat(Float.random(in: 0...1)), blue:
-            CGFloat(Float.random(in: 0...1)), alpha: 0)
+        view.backgroundColor = UIColor(white: 1, alpha: 0)
     }
     
     func fadeIn(view: UIView){
         view.transform = CGAffineTransform(scaleX: 1, y: 1)
-        view.backgroundColor = UIColor(displayP3Red: CGFloat(Float.random(in: 0...1)), green: CGFloat(Float.random(in: 0...1)), blue:
-            CGFloat(Float.random(in: 0...1)), alpha: 1)
+        view.backgroundColor = UIColor(displayP3Red: red, green: green, blue:
+            blue, alpha: 1)
     }
     
     func move(view: UIView) {
@@ -49,12 +50,16 @@ class ViewController: UIViewController {
     }
     
     @objc func checkAction(sender : UITapGestureRecognizer) {
+        self.red = CGFloat(Float.random(in: 0...1))
+        self.blue = CGFloat(Float.random(in: 0...1))
+        self.green = CGFloat(Float.random(in: 0...1))
+        
         UIView.animate(withDuration: 0.5, animations: {
             self.fadeOut(view: self.bubble)
         }) { (finished) in
             if finished {
-                UIView.animate(withDuration: 0.5, delay: 0.5, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveLinear, animations: {
-                    self.move(view: self.bubble)
+                self.move(view: self.bubble)
+                UIView.animate(withDuration: 1, delay: 0.5, usingSpringWithDamping: 0.46, initialSpringVelocity: 0.54, options: .curveLinear, animations: {
                     self.fadeIn(view: self.bubble)
                 })
             }
